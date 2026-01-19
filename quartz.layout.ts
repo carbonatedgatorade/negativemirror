5758832fcabfc74a061111aec2ebabd6131c3438
jade <jdbe@protonmail.com>
2026-01-18 17:27:20 -0800

    Quartz sync: Jan 18, 2026, 5:27 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -22,7 +22,7 @@ export const defaultContentPageLayout: PageLayout = {
       condition: (page) => page.fileData.slug !== "index",
     }),
     Component.ArticleTitle(),
-    Component.ContentMeta(showReadingTime: false),
+    Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
